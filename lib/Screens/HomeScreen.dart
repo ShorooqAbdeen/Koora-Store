e1792453321ab51d0a1c1c31b380565ef51f6b9a
Sama-3li-2002 <samaaliallham@gmail.com>
2023-01-31 19:17:14 +0200

    the latest committ

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+
+import '../DrawerScreens/DrawerWidget.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -12,79 +14,8 @@ class HomeScreen extends StatelessWidget {
             elevation: 5,
             centerTitle: true,
           ),
-          drawer: NavigationDrawers(),
-        )
+          drawer:DrawerWidget(),
+    )
     );
   }
 }
-class NavigationDrawers extends StatefulWidget {
-  @override
-  State<NavigationDrawers> createState() => _NavigationDrawersState();
-}
-
-class _NavigationDrawersState extends State<NavigationDrawers> {
-  @override
-  Widget build(BuildContext context) {
-    return Drawer(
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            BuildHeader(context),
-            BuildMenuItems(context),
-          ],
-        ),
-      ),
-    );
-  }
-}
-Widget BuildHeader(BuildContext context)=> SizedBox(
-  height: 250,
-  child:DrawerHeader(
-  margin: EdgeInsets.all(0.0),
-  padding: EdgeInsets.all(0.0),
-  decoration: BoxDecoration(
-      image: DecorationImage(
-        fit: BoxFit.cover,
-        image:AssetImage("images/koora_logo.png"),
-      ),
-      color: Color(0xff1C1C1C)
-  ),
-  child: ListView(
-    padding: EdgeInsets.only(top: 175,),
-    children: [
-      SizedBox(height: 30,
-       child: Text("KooraStore@store.com",style: TextStyle(color: Colors.grey),),),
-    ],
-  ),
-),
-);
-
-Widget BuildMenuItems(BuildContext context)=> Column(
-  children: [
-    ListTile(
-      leading: Icon(Icons.settings,color: Colors.black,),
-      title: Text("Settings"),
-      onTap: (){},
-    ),
-    SizedBox(height: 5,),
-    ListTile(
-      leading: Icon(Icons.account_box,color: Colors.black,),
-      title: Text("Accounts"),
-    ),
-    SizedBox(height:5,),
-    ListTile(
-      leading: Icon(Icons.question_mark,color: Colors.black,),
-      title: Text("Any issues"),
-    ),
-    SizedBox(height: 5,),
-    ListTile(
-      leading: Icon(Icons.logout,color: Colors.black,),
-      title: Text("Logout"),
-    ),
-  ],
-);
-
-
-
-
-
